62ade14e9b3e027a3eb7a05cd8e5c1b1bf5abe74
Adriano Emerick Cola <adrianocola@gmail.com>
2014-11-13 02:22:19 -0200

    Adicionado usuário de admin para conectar no mongodb

--- protected/models/index.js
+++ protected/models/index.js
@@ -2,7 +2,16 @@
 app.models = {};
 
 var mongoose = app.mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/zombies');
+
+if(process.env.NODE_ENV === "prod"){
+
+    mongoose.connect('mongodb://admin:admin@localhost/zombies');
+
+}else{
+    mongoose.connect('mongodb://localhost/zombies');
+}
+
+
 
 module.exports = require('require-directory')(module);
 
